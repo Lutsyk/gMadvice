e5684ce466c85e44e6c2e5c394715e4836168f32
Max Lutsenko <makc199710@me.com>
2021-11-06 23:48:48 +0100

    Sprint 3
    Add:
    - Array for advice (for translating)
    - count forRandomAdvice
    - Random function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const adviceArea = document.querySelector('.advice-slip');
 const submitBtn = document.querySelector('#btn')
 const input = document.querySelector('#numberOfAdvice')
 const screen1 = document.querySelector('.screen1')
+let countAdvice = 0
+let adviceArray = []
 
 input.addEventListener('change', event => {
     
@@ -25,3 +27,9 @@ submitBtn.addEventListener('click', event => {
     screen1.classList.add('up');
     getSeveralAdvice(countAdvice)
 })
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
